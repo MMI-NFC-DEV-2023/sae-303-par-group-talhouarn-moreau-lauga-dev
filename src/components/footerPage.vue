e3d63b0bf0991b256f13192d0f26c4fbd5cd3bb5
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-10-23 14:07:24 +0200

    ajout nom-prenom dans footer

--- src/components/footerPage.vue
+++ src/components/footerPage.vue
@@ -6,7 +6,7 @@ import { RouterLink } from 'vue-router'
 <template>
   <footer class="pb-10 pt-10 lg:space-y-0 bg-gray-950">
     
-      <p class="text-orange-100 text-center px-10 text-xs lg:text-base">Projet réalisé dans le cadre d’un exercice pédagogique au département MMi de Montbéliard qui  répond à un des défis de l’Open Data University, propulsé par l’association Latitudes</p>
-      
+      <p class="text-orange-50 mb-4 text-center px-10 text-xs lg:text-base">Projet réalisé dans le cadre d’un exercice pédagogique au département MMi de Montbéliard qui  répond à un des défis de l’Open Data University, propulsé par l’association Latitudes</p>
+      <p class="text-orange-50 text-center px-10 text-xs lg:text-base ">Réalisé par Clemence Lauga, Gabriel Moreau et Noélie Talhouarn</p>
   </footer>
 </template>
