6d0f0f1276a3ca36828229dc086244b5b92f6c42
Noelie-Talhouarn <noelie.talhouarn@edu.univ-fcomte.fr>
2024-10-23 14:09:35 +0200

    modif

--- src/pages/prevision.vue
+++ src/pages/prevision.vue
@@ -1,7 +1,46 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
+    <section class="p-7 lg:px-36">
+    <div>
   <h1>Les prévisions</h1>
+  <p>
+    Les changements climatiques que nous observons aujourd'hui ne sont que le début d'une
+    transformation plus profonde et plus durable de notre environnement. Grâce aux données réelles
+    et prévisionnelles, il est possible d'anticiper l'évolution des conditions météorologiques,
+    notamment la hausse des températures.
+  </p>
+  </div>
+  <div>
+  <p>graph</p>
+  <h3 class="mb-3">Interprétation des données</h3>
+    <ul class="list-disc list-inside">
+      <li class="mb-2">
+       Données réelles (en rouge) : De 2005 jusqu'à environ 2025, les points rouges montrent l'évolution des températures mesurées. On observe une tendance générale à la hausse, malgré quelques fluctuations à la baisse dans certaines années. La température moyenne est passée d'environ 12,3°C en 2005 à environ 13,6°C en 2025. C'est une augmentation significative en 20 ans.
+      </li>
+      <li class="mb-2">
+       Données prévisionnelles (en jaune) : Après 2025, les points jaunes représentent les prévisions de température moyenne jusqu'en 2050. La courbe montre une montée continue des températures avec une augmentation progressive qui projette des températures autour de 14,8°C à 15°C d'ici 2050. Les prévisions soulignent une tendance haussière sans signe de stabilisation.
+      </li>
+    </ul>
+  <h3 class="mb-3">Analyse des tendances</h3>
+    <ul class="list-disc list-inside">
+      <li class="mb-2">
+        Hausse constante des températures : La température moyenne est en constante augmentation, ce qui reflète les impacts du changement climatique en France. La hausse de température pourrait être due à plusieurs facteurs, tels que l'augmentation des gaz à effet de serre et les activités humaines.
+      </li>
+      <li class="mb-4">
+        Différences entre les données réelles et prévisionnelles : Si les données réelles montrent des fluctuations d'année en année, les prévisions semblent indiquer une croissance plus stable et régulière. Cela peut refléter une modélisation basée sur des scénarios climatiques futurs.
+      </li>
+    </ul>
+    <h3 class="mb-3">Implications</h3>
+    <ul class="list-disc list-inside">
+      <li class="mb-2">
+        Changement climatique : Cette augmentation des températures est alarmante car elle peut accentuer les phénomènes météorologiques extrêmes, comme les vagues de chaleur, et avoir des effets importants sur l'agriculture, la biodiversité et les ressources en eau.
+      </li>
+      <li class="mb-2">
+        Réponse politique et sociale : La courbe montre clairement que des actions sont nécessaires pour limiter cette augmentation et ses effets néfastes. Les décideurs peuvent utiliser ce genre de données pour planifier des politiques de réduction des émissions de CO2 et d’adaptation climatique.
+      </li>
+    </ul>
+    <p>https://explore.data.gouv.fr/fr/datasets/6569af36ba0c3d2f9d4bf98c/#/resources/33dc01bc-1c8b-4e5d-bfcd-185ffe2db682</p>
+  </div>
+  </section>
 </template>
